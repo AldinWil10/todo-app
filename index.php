c621cdd9ef91bc215b190e15e46ad13062e4b1c3
Aldin Wildan <08.aldinwildan@gmail.com>
2024-03-16 15:36:16 +0700

    feat: membuat template html untuk app

---
+++ index.php
@@ -0,0 +1,23 @@
+<h1>Todo App</h1>
+<form action="">
+    <label>Daftar Belanja Hari ini</label>
+    <input type="text" name="todo">
+    <button type="submit">Simpan</button>
+</form>
+<ul>
+    <li>
+        <input type="checkbox" name="todo">
+        <label>Todo 1</label>
+        <a href="#">Hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo">
+        <label>Todo 2</label>
+        <a href="#">Hapus</a>
+    </li>
+    <li>
+        <input type="checkbox" name="todo">
+        <label>Todo 3</label>
+        <a href="#">Hapus</a>
+    </li>
+</ul>
