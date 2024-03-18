2e59e8d5cd24a54712fd42dd4d06451169f764ce
Aldin Wildan <08.aldinwildan@gmail.com>
2024-03-18 16:50:34 +0700

    feat: menambahkan fitur delete serta menyelesaikan aplikasi

--- index.php
+++ index.php
@@ -1,38 +1,56 @@
 <?php
 $todo = [];
-$file = file_get_contents('todo.txt');
-$todos = unserialize($file);
+if (file_exists('todo.txt')) {
+    $file = file_get_contents('todo.txt');
+    $todos = unserialize($file);
+}
 
-if (isset($_POST['todo']))
+if (isset($_POST['todo'])) {
     $data =  $_POST['todo'];
-$todos[] = [
-    'todo'    => $data,
-    'status' => 0
-];
-file_put_contents('todo.txt', serialize($todos));
+    $todos[] = [
+        'todo'    => $data,
+        'status' => 0
+    ];
+    file_put_contents('todo.txt', serialize($todos));
+    header('location:index.php');
+}
+
+if (isset($_GET['status'])) {
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    file_put_contents('todo.txt', serialize($todos));
+    header('location:index.php');
+}
+
+if (isset($_GET['hapus'])) {
+    unset($todos[$_GET['key']]);
+    file_put_contents('todo.txt', serialize($todos));
+    header('location:index.php');
+}
+
+print_r($todos);
 ?>
 
 
 <h1>Todo App</h1>
 <form method="post">
-    <label>Daftar Belanja Hari ini</label>
+    <label>Kegiatan Hari ini</label><br>
     <input type="text" name="todo">
     <button type="submit">Simpan</button>
 </form>
 <ul>
-    <li>
-        <input type="checkbox" name="todo">
-        <label>Todo 1</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label>Todo 2</label>
-        <a href="#">Hapus</a>
-    </li>
-    <li>
-        <input type="checkbox" name="todo">
-        <label>Todo 3</label>
-        <a href="#">Hapus</a>
-    </li>
+    <?php foreach ($todos as $key => $value) : ?>
+        <li>
+            <input type="checkbox" name="todo" onclick="window.location.href='index.php?status=<?php echo ($value['status'] == 1) ? '0' : '1'; ?>&key=<?php echo $key; ?>'" ; <?php if ($value['status'] == 1) echo 'checked' ?>>
+            <label>
+                <?php
+                if ($value['status'] == 1) {
+                    echo '<del>' . $value['todo'] . '</del>';
+                } else {
+                    echo $value['todo'];
+                }
+                ?>
+            </label>
+            <a href="index.php?hapus=1&key=<?php echo $key; ?>" onclick="return confirm('Apakah Anda Yakin akan menghapus data ini?')">hapus</a>
+        </li>
+    <?php endforeach; ?>
 </ul>
